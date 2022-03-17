e1a5cc69a6e12bf26f7ac3005be8fc1715b3f146
aamir <mallickalam44@gmail.com>
2022-03-16 23:18:33 -0400

    Spring THYMELEAF

---
+++ Spring-09-Thymleaf/src/main/java/com/example/Model/Student.java
@@ -0,0 +1,14 @@
+package com.example.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+
+public class Student {
+    private String firstName,lastName;
+    private int age;
+
+    private String state;
+}
---
+++ Spring-09-Thymleaf/src/test/java/com/example/Model/Spring09ThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09ThymleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
