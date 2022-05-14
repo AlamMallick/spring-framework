02c07a18e1e989b5174c7f0aa0b9f606147a6a87
aamir <mallickalam44@gmail.com>
2022-05-14 15:13:49 -0400

    CinemaLab

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/example/entity/Cinema.java
@@ -0,0 +1,21 @@
+package com.example.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+
+public class Cinema extends BaseEntity {
+    private String name;
+    private String sponsoredName;
+
+    private Location location;
+
+
+
+}
