2e0aef6c606776120edb779c4128fffdbe598856
aamir <mallickalam44@gmail.com>
2022-06-07 20:44:54 -0400

    Response Entity

--- Spring-17-REST-ConsumingAPIS/src/main/java/com/cydeo/controller/Consume_RESTtemplate.java
+++ Spring-17-REST-ConsumingAPIS/src/main/java/com/cydeo/controller/Consume_RESTtemplate.java
@@ -3,6 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.dto.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -29,6 +30,17 @@ public class Consume_RESTtemplate {
       ResponseEntity<User[]>  responseEntity= restTemplate.getForEntity(Uri,User[].class);  //json object and it to dto
 
         return responseEntity.getBody();
+
+    }
+
+
+
+    @GetMapping("{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+        String URL=Uri+"/{id}";
+
+        return restTemplate.getForObject(URL,Object.class,id);
+
 
     }
 }
