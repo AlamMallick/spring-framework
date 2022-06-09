d3589df6e4413ea41f06b303aa74ed6041f1c64f
aamir <mallickalam44@gmail.com>
2022-06-08 20:54:34 -0400

    Response Entity

--- Spring-17-REST-ConsumingAPIS/src/main/java/com/cydeo/controller/Consume_OpenFeign.java
+++ Spring-17-REST-ConsumingAPIS/src/main/java/com/cydeo/controller/Consume_OpenFeign.java
@@ -1,6 +1,31 @@
 package com.cydeo.controller;
 
+import com.cydeo.client.EmployeeClient;
+import com.cydeo.client.UserClient;
+import com.cydeo.dto.ResponseWrapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class Consume_OpenFeign {
+    private final UserClient userClient;
+    private final EmployeeClient employeeClient;
+
+
+    public Consume_OpenFeign(UserClient userClient, EmployeeClient employeeClient) {
+        this.userClient = userClient;
+        this.employeeClient = employeeClient;
+    }
+
+    @GetMapping("/api/v1/users")
+    public ResponseEntity<ResponseWrapper> getUsers(){
+
+        return ResponseEntity.ok(new ResponseWrapper("UserList Retrieved",userClient.getUsers()));
+    }
+
+    @GetMapping("/api/v1/employee")
+    public ResponseEntity<ResponseWrapper> getEmployee(){
+        return ResponseEntity.ok(new ResponseWrapper("Successfully retrieved ",employeeClient.getEmployee("6298ebfecd0551211fce37a6")));
+    }
 
 
 }
